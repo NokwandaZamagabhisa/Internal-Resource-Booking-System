a06c87c3e42d5d04d1e39b70a051c269d4629f1d
NokwandaZamagabhisa <Zamagabhisa23@gmail.com>
2025-07-16 11:41:14 +0200

    Create an ApplicationDbContext class

--- BookingSystem.DataAccess/ApplicationDbContext.cs
+++ BookingSystem.DataAccess/ApplicationDbContext.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using ResourceBooking.Core.Entities;
+
+namespace BookingSystem.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+
+
+    }
+}
